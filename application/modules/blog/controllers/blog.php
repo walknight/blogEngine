afd1adce8138a629f8c2a12d7986dcd46141dc62
walknight <darkwalkernight@gmail.com>
2015-08-04 09:44:28 +0700

    update script

--- application/modules/blog/controllers/blog.php
+++ application/modules/blog/controllers/blog.php
@@ -1,4 +1,5 @@
-<?php
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 class Blog extends MX_Controller
 {
 	protected $_template;
@@ -56,7 +57,7 @@ class Blog extends MX_Controller
 		}
 		else
 		{
-			$this->_template['page']	= 'errors/no_posts';
+            $this->_template['page']	= 'errors/no_posts';
 		}
 
 		$this->system_library->load($this->_template['page'], $data);
